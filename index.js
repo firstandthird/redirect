ffff62cf21b13a6f2427665b12dde24d2aa0e804
Greg Allen <greg@firstandthird.com>
2017-04-13 15:32:07 -0700

    a few updates

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 const redirect = process.env.REDIRECT || process.argv.pop();
 const statusCode = process.env.STATUS || 301;
-const verbose = process.env.VERBOSE;
 const url = require('url');
 const Logr = require('logr');
 const logrFlat = require('logr-flat');
 const log = Logr.createLogger({
   reporters: {
     flat: {
-      reporter: logrFlat
+      reporter: logrFlat,
+      options: {
+        appColor: true
+      }
     }
   }
 });
@@ -19,20 +21,20 @@ const log = Logr.createLogger({
 if (!redirect) {
   throw new Error('must set REDIRECT env var');
 }
+log(['redirect', 'start'], `Redirecting to ${redirect}`);
 const server = http.createServer((req, res) => {
   const fullurl = url.resolve(redirect, req.url);
   res.writeHead(statusCode, {
     Location: fullurl
   });
-  if (verbose) {
-    log({
-      from: `${req.headers.host}${req.url}`,
-      to: fullurl,
-      referral: req.headers.referer || ''
-    });
-  }
+  log(['redirect'], {
+    from: `${req.headers.host}${req.url}`,
+    to: fullurl,
+    referral: req.headers.referer || ''
+  });
   res.end();
 }).listen(port);
+
 process.on('SIGTERM', () => {
   server.close(() => {
     process.exit(0);
