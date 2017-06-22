deaeb426847e6dc41e349504077b77035e391243
Chris <orthagonal1@gmail.com>
2017-06-21 23:35:18 -0500

    redirect can be 'https' or 'www' as well, includes tests

--- index.js
+++ index.js
@@ -20,18 +20,58 @@ const log = Logr.createLogger({
 
 let server;
 
+module.exports.getRedirect = (redirect, req) => {
+  let fullurl;
+  switch (redirect) {
+    /// www attempts to remove the 'www.' portion at the start of a url:
+    case 'www':
+      fullurl = url.format({
+        protocol: 'http',
+        host: req.headers.host,
+        pathname: req.url
+      });
+      if (!fullurl.startsWith('http://www')) {
+        log(['redirect', 'error'], `${fullurl} does not start with "www"`);
+        return undefined;
+      }
+      return fullurl.replace('www.', '');
+    // https attempts to redirect to the https version of the requested host
+    case 'https':
+      return url.format({
+        protocol: 'https',
+        host: req.headers.host,
+        pathname: req.url
+      });
+    // otherwise just redirect to the specified redirect host:
+    default:
+      return url.resolve(redirect, req.url);
+  }
+};
+
 module.exports.start = (redirect) => {
   log(['redirect', 'start'], `Port ${port} redirecting to ${redirect}`);
   server = http.createServer((req, res) => {
-    const fullurl = url.resolve(redirect, req.url);
-    res.writeHead(statusCode, {
-      Location: fullurl
-    });
-    log(['redirect'], {
-      from: `${req.headers.host}${req.url}`,
-      to: fullurl,
-      referral: req.headers.referer || ''
-    });
+    const fullurl = module.exports.getRedirect(redirect, req);
+    // write the redirect header and log that we're redirecting
+    if (fullurl) {
+      res.writeHead(statusCode, {
+        Location: fullurl
+      });
+      log(['redirect'], {
+        from: `${req.headers.host}${req.url}`,
+        to: fullurl,
+        referral: req.headers.referer || ''
+      });
+    } else {
+      // otherwise it's a Bad Request error:
+      res.writeHead(400, {});
+      log(['redirect', 'error'], {
+        message: 'could not process redirect',
+        redirect,
+        from: `${req.headers.host}${req.url}`,
+        referral: req.headers.referer || ''
+      });
+    }
     res.end();
   }).listen(port);
 };
--- tests/test.redirect.js
+++ tests/test.redirect.js
@@ -3,16 +3,32 @@ const tap = require('tap');
 const redirect = require('../index.js');
 const wreck = require('wreck');
 
-tap.afterEach((done) => {
-  redirect.stop(done);
-});
-
-tap.test('test redirect here', (t) => {
+tap.test('redirects to the redirect location', (t) => {
   redirect.start('http://google.com');
   wreck.get('http://localhost:8080/destination', (err, res, payload) => {
     t.equal(err, null, 'does not error when called');
     t.equal(res.statusCode, 301, 'returns HTTP 301');
     t.equal(res.headers.location, 'http://google.com/destination', 'returns correct location header');
-    t.end();
+    redirect.stop(t.end);
+  });
+});
+
+tap.test('getRedirect redirects to https', (t) => {
+  const redirection = redirect.getRedirect('https', { headers: { host: 'origin.com' }, url: '/destination' });
+  t.equal(redirection, 'https://origin.com/destination', 'redirects to https');
+  t.end();
+});
+
+tap.test('getRedirect strips "www."', (t) => {
+  const redirection = redirect.getRedirect('www', { headers: { host: 'www.origin.com' }, url: '/destination' });
+  t.equal(redirection, 'http://origin.com/destination', 'replaces the "www" portion');
+  t.end();
+});
+
+tap.test('returns 400 Bad Request when redirect is www but host did not start with "www."', (t) => {
+  redirect.start('www');
+  wreck.get('http://localhost:8080/destination', (err, res, payload) => {
+    t.equal(err.output.statusCode, 400, 'error is HTTP 400');
+    redirect.stop(t.end);
   });
 });
